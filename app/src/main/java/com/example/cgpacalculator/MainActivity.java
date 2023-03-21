19d184625c4e9bff1cf9bb906c25d1fa6456eaec
Nakshatra Gupta <nakshatragupta007@gmail.com>
2023-03-21 16:17:30 +0530

    Dark Mode Disabled

--- app/src/main/java/com/example/cgpacalculator/MainActivity.java
+++ app/src/main/java/com/example/cgpacalculator/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.cgpacalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
 import android.text.Editable;
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
        /*Spinner spinner = (Spinner) findViewById(R.id.spinner);
